5d0ed332fc9d34b5e2d7e543040852cc02bfce44
Rémy Pelletier <remypelletier2@gmail.com>
2023-12-15 02:03:33 +0100

    feat: add api plateform

--- symfony/src/Entity/Broker.php
+++ symfony/src/Entity/Broker.php
@@ -4,8 +4,10 @@ namespace App\Entity;
 
 use App\Repository\BrokerRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
 #[ORM\Entity(repositoryClass: BrokerRepository::class)]
+#[ApiResource]
 class Broker
 {
     #[ORM\Id]
